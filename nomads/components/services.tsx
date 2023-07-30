f68d2d8351df8a42a49285721e21340be282e121
NurkaAmre <nurkerey30@gmail.com>
2023-07-30 15:48:56 +0600

    Services section

--- nomads/components/services.tsx
+++ nomads/components/services.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { FaPlane, FaHotel, FaTruck, FaBed, } from 'react-icons/fa';
 import { AiOutlineMenu } from 'react-icons/ai';
+import Image from 'next/image';
+import bus from '@/public/bus.png'
+import hotel from '@/public/hotel.png'
+import travel from '@/public/travel.png'
+import meal from '@/public/meal.png'
 
 const Services = () => {
   return (
@@ -8,15 +13,24 @@ const Services = () => {
       <div className='flex flex-col text-white justify-center items-center'>
         <h2 className='text-center mb-3 text-4xl md:text-5xl font-abhaya'>The Perfect Travel</h2>
         <hr className='w-[180px] md:w-[720px] h-px mx-auto mt-2 md:mb-2 bg-gray-500 border-0 rounded dark:bg-gray-700' />
-        <p className='mx-6 my-4 md:text-lg font-merri'>
+        <p className='mx-6 my-4 text-xl font-aloe md:text-lg'>
           We cover all expences for picking the perfect hotel to flight and destination.
         </p>
       </div>
-      <div className='bg-white flex py-6 flex-col gap-3 md:flex-row'>
+      <div className='bg-white flex py-6 flex-col justify-center md:flex-row'>
         <div className='mx-[2rem] shadow-lg'>
-          <div className='flex justify-evenly py-2 bg-[#1B1D33]'>
-            <FaPlane className='text-[#6491C9]' />
-            <p className='text-white font-aloe'>Tour Guide</p>
+          <div className='flex justify-center gap-5 py-2 bg-[#1B1D33]'>
+            <Image src={travel} alt='travel' width={30} height={30} />
+            <p className='text-white font-abril'>Tour Guide</p>
+          </div>
+            <p className='p-4'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magnam ut ea error perspiciatis magni velit unde tempore quasi? Dignissimos?
+          </p>
+        </div>
+        <div className='mx-[2rem] shadow-lg'>
+          <div className='flex justify-center gap-5 py-2 bg-[#1B1D33]'>
+            <Image src={hotel} alt='hotel' width={30} height={30} />
+            <p className='text-white font-abril'>Hotel</p>
           </div>
           <div>
             <p className='p-4'>
@@ -25,9 +39,9 @@ const Services = () => {
           </div>
         </div>
         <div className='mx-[2rem] shadow-lg'>
-          <div className='flex justify-evenly py-2 bg-[#1B1D33]'>
-            <FaPlane className='text-[#6491C9]' />
-            <p className='text-white font-aloe'>Tour Guide</p>
+          <div className='flex justify-center gap-5 py-2 bg-[#1B1D33]'>
+            <Image src={bus} alt='bus' width={30} height={30} />
+            <p className='text-white font-abril'>Transport</p>
           </div>
           <div>
             <p className='p-4'>
@@ -36,20 +50,9 @@ const Services = () => {
           </div>
         </div>
         <div className='mx-[2rem] shadow-lg'>
-          <div className='flex justify-evenly py-2 bg-[#1B1D33]'>
-            <FaPlane className='text-[#6491C9]' />
-            <p className='text-white font-aloe'>Tour Guide</p>
-          </div>
-          <div>
-            <p className='p-4'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis magnam ut ea error perspiciatis magni velit unde tempore quasi? Dignissimos?
-            </p>
-          </div>
-        </div>
-        <div className='mx-[2rem] shadow-lg'>
-          <div className='flex justify-evenly py-2 bg-[#1B1D33]'>
-            <FaPlane className='text-[#6491C9]' />
-            <p className='text-white font-aloe'>Tour Guide</p>
+          <div className='flex justify-center gap-5 py-2 bg-[#1B1D33]'>
+            <Image src={meal} alt='meal' width={30} height={30} />
+            <p className='text-white font-abril'>Meal</p>
           </div>
           <div>
             <p className='p-4'>
